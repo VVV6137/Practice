6771dbd05bf3e8874702b879029ac13bd3f9d5b4
VVV6137 <vasiletsvasya61@gmail.com>
2025-09-25 22:04:54 +0800

    Update Задание 3.py

--- 3/Задание 3.py
+++ 3/Задание 3.py
@@ -2,11 +2,11 @@ def snow(n, k, events):
     snow = [0] * (n + 1)
     results = []
     for event in events:
-        event_type = event[0]        
+        event_type = event[0]
         if event_type == 1:
             street_index = event[1]
             snow_amount = event[2]
-            snow[street_index] += snow_amount            
+            snow[street_index] += snow_amount
         elif event_type == 2:
             start_index = event[1]
             end_index = event[2]
@@ -14,9 +14,9 @@ def snow(n, k, events):
             results.append(total_snow)
     return results
 
-if __name__ == "__main__":
-    n, k = map(int, input().split())
-    events = [list(map(int, input().split())) for _ in range(k)]
-    results = snow(n, k, events)
-    for result in results:
-        print(result)
+n, k = map(int, input().split())
+events = [list(map(int, input().split())) for _ in range(k)]
+results = snow(n, k, events)
+for result in results:
+    print(result)
+
