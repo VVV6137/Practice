24f1c26e4730ce463177eb9a5281c648351d7221
VVV6137 <vasiletsvasya61@gmail.com>
2025-09-26 11:17:34 +0800

    Update Задание 6.py

--- 6/Задание 6.py
+++ 6/Задание 6.py
@@ -1,11 +1,19 @@
-def pairs(n, test_cases):
+def pairs(n, t):
     results = []
-    for a, b, x, y in test_cases:
+    for a, b, x, y in t:
         pairs = min(a + b, x + y, min(a, x + y) + min(b, x))
         results.append(pairs)
     return results
 
 n = int(input())
-test_cases = [tuple(map(int, input().split())) for _ in range(n)]
-results = pairs(n, test_cases)
-print(' '.join(map(str, results)))
+t = []
+for _ in range(n):
+    l = input().split()
+    l = tuple(map(int,l))
+    t.append(l)
+results = pairs(n, t)
+r = ""
+for i in results:
+    r += str(i)
+    r += " "
+print(r)
