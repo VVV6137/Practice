780491689690e52a400c3a29ab7b2d7bf0d15aea
VVV6137 <vasiletsvasya61@gmail.com>
2025-09-26 11:06:05 +0800

    Update Задание 4.py

--- 4/Задание 4.py
+++ 4/Задание 4.py
@@ -1,20 +1,24 @@
-def inverse(permutation):
-    n = len(permutation)
+def inverse(p):
+    n = len(p)
     inv = [0] * n
     for i in range(n):
-        inv[permutation[i] - 1] = i + 1
+        inv[p[i] - 1] = i + 1
     return inv
 
-def decrypt(encrypted_word, permutation, k):
-    n = len(encrypted_word)
-    inv = inverse(permutation)
-    current_word = encrypted_word
+def decrypt(word_1, p, k):
+    n = len(word_1)
+    inv = inverse(p)
+    curr_word = word_1
     for _ in range(k):
-        current_word = ''.join(current_word[inv[i] - 1] for i in range(n))
-    return current_word
+        curr_word = ''.join(curr_word[inv[i] - 1] for i in range(n))
+    return curr_word
 
-n, k = map(int, input().split())
-permutation = list(map(int, input().split()))
-encrypted_word = input().strip()
-original_word = decrypt(encrypted_word, permutation, k)
-print(original_word)
+l = input().split()
+n = int(l[0])
+k = int(l[1])
+l1 = input().split()
+p = list(map(int,l1))
+word_1 = input().strip()
+word_0 = decrypt(word_1, p, k)
+print(word_0)
+
