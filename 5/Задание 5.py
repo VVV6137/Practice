c46cb488ac489b86cbbb758997829c44682754ea
VVV6137 <vasiletsvasya61@gmail.com>
2025-09-26 11:11:57 +0800

    Update Задание 5.py

--- 5/Задание 5.py
+++ 5/Задание 5.py
@@ -1,9 +1,9 @@
-def rectangle(matrix, h, w):
+def rectangle(m, h, w):
     min_row, min_col = h, w
     max_row, max_col = -1, -1
     for i in range(h):
         for j in range(w):
-            if matrix[i][j] == 1:
+            if m[i][j] == 1:
                 if i < min_row:
                     min_row = i
                 if j < min_col:
@@ -14,8 +14,15 @@ def rectangle(matrix, h, w):
                     max_col = j
     return min_row, min_col, max_row, max_col
 
-h, w = map(int, input().split())
-matrix = [list(map(int, input().split())) for _ in range(h)]
-top_left_row, top_left_col, bottom_right_row, bottom_right_col = rectangle(matrix, h, w)
-print(top_left_row - 1, top_left_col - 1, bottom_right_row + 1, bottom_right_col + 1)
+l = input().split()
+h = int(l[0])
+w = int(l[1])
+m = []
+for _ in range(h):
+    l = input().split()
+    l = list(map(int,l))
+    m.append(l)
+a, b, c, d = rectangle(m, h, w)
+print(a - 1, b - 1, c + 1, d + 1)
+
 
